9963e67dfcc40299873804c6a8c30068e94e8561
Andrey Sirota <sirota.av@gmail.com>
2022-01-21 10:54:09 +0500

    Рассчитать скидку

--- task_2/solution.js
+++ task_2/solution.js
@@ -1,7 +1,7 @@
 function calcShipping(sum, min, shipping) {
-    let productsSum = sum; // сумма в корзине
-    let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
-    let shippingPrice = shipping; // стоимость доставки
+    let productsSum = sum;          // сумма в корзине
+    let freeShippingMinSum = min;   // минимальная цена для бесплатной доставки
+    let shippingPrice = shipping;   // стоимость доставки
 
     // Задание №2.1. Рассчитать доставку
 
@@ -10,11 +10,12 @@ function calcShipping(sum, min, shipping) {
         shippingSum = 0;        // то shippingSum присвоить значение 0
     }
     if (productsSum >= freeShippingMinSum) {    // если productsSum Больше или равна freeShippingMinSum,
-        freeShippingMinSum = 0                  // то shippingSum присвоить значение 0
+        shippingSum = 0                         // то shippingSum присвоить значение 0
         
     }
     if (productsSum >0 && productsSum < freeShippingMinSum) {   // если productsSum больше 0 и меньше freeShippingMinSum,
-        shippingSum = shippingPrice }                           // то shippingSum присвоить значение shippingPrice
+        shippingSum = shippingPrice                             // то shippingSum присвоить значение shippingPrice
+    }                           
 
     // Конец решения задания №2.1.
 
@@ -22,17 +23,23 @@ function calcShipping(sum, min, shipping) {
 }
 
 function calcDiscount(sum, min, discount) {
-    let productsSum = sum; // сумма в корзине
-    let discountMinSum = min; // минимальная цена для скидки
-    let discountPart = discount; // величина скидки в процентах
+    let productsSum = sum;          // сумма в корзине
+    let discountMinSum = min;       // минимальная цена для скидки
+    let discountPart = discount;    // величина скидки в процентах
 
     // Задание №2.2. Рассчитать скидку
 
-    // создайте переменную discountSum
+    let discountSum                             // создайте переменную discountSum
+    if (productsSum >= discountMinSum) {        // если productsSum больше или равно discountMinSum,
+       discountSum = discountPart;              // то присвойте discountSum значение discountPart процентов от productsSum,
+       else discountSum = 0;
 
-    // если productsSum больше или равно discountMinSum,
-    // то присвойте discountSum значение discountPart процентов от productsSum,
-    // иначе присвойте discountSum значение 0
+
+    }
+                        // иначе присвойте discountSum значение 0
+    
+   
+    
 
     // Конец решения задания №2.2.
 
