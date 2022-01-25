2e5845d66e013715f8422150b62d8d1b329d1583
Andrey Sirota <sirota.av@gmail.com>
2022-01-25 15:43:19 +0500

    Формирование строки корзины

--- task_3/solution.js
+++ task_3/solution.js
@@ -17,7 +17,7 @@ function renderCartItem(item) {
     let itemCountText;                         // 1. Создайте переменную itemCountText, равную пустой строке
     itemCountText = itemCountText + product.count;  // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
     itemCountText = itemCountText + ` x`;           // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
-    itemCountText = itemCountText + produc.price;   // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
+    itemCountText = itemCountText + product.price;   // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
     itemCountText = itemCountText + ` ₽ = `;        // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
     let sum = product.count * product.price;        // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
     itemCountText = itemCountText + sum;            // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
